d447d2b5605ef7503e8781efc49c012c693d5182
Cheranga Hatangala <cheranga@gmail.com>
2020-12-05 09:43:20 +1100

    Add project files.

---
+++ MyShop.ProductManagement.Api/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.ProductManagement.Api.Requests;
+
+namespace MyShop.ProductManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> PostProduct(CreateProductRequest createProductRequest)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2)).ConfigureAwait(false);
+
+            return CreatedAtAction(nameof(GetProductById), new { productId = createProductRequest.ProductId }, createProductRequest.ProductId);
+        }
+
+        [HttpGet("{productId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetProductById(string productId)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2)).ConfigureAwait(false);
+
+            return Ok(new
+            {
+                id = productId
+            });
+        }
+    }
+}
---
+++ MyShop.ProductManagement.Api/Requests/CreateProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShop.ProductManagement.Api.Requests
+{
+    public class CreateProductRequest
+    {
+        public string ProductId { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+    }
+}
