ab48cc9ca5a3a436c56202a6b8e7740a85d1f601
Cheranga Hatangala <cheranga@gmail.com>
2021-01-07 12:02:20 +1100

    Organized into projects initially.

---
+++ MyShop.ProductManagement.Services/IProductsService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MyShop.ProductManagement.Core;
+using MyShop.ProductManagement.DataAccess.Models;
+using MyShop.ProductManagement.Services.Requests;
+
+namespace MyShop.ProductManagement.Api.Services
+{
+    public interface IProductsService
+    {
+        Task<Result<int>> UpsertProductAsync(UpsertProductRequest request);
+        Task<Result<ProductDataModel>> GetProductAsync(GetProductRequest request);
+    }
+}
